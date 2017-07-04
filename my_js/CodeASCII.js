9a2818a20ecabf9247d5cc9c5fd0f3b61e8896ef
vannidelprete <pibe11@hotmail.it>
2017-07-04 16:35:16 +0200

    jQuery on ASCII app

--- my_js/CodeASCII.js
+++ my_js/CodeASCII.js
@@ -1,10 +1,21 @@
-function codeChar(e) {
-    var charMsg;
-    charMsg = document.getElementById('reponse');
-    charMsg.innerHTML = "Il codice ASCII di " + String.fromCharCode(e.keyCode) +
-        " &egrave: "+ e.keyCode || e.charCode; //inserisci l'output'
-}
+$(function() {
+    var $inputChar = $('input:text');           //puntatore al nodo di testo del tag input
+    var $startConvert = $('#startConvert');     //puntatore al form
+    var $reponse = $('ul#reponse');             //puntatore al risultato del convertitore
+   
 
-var elInput = document.getElementById('character');
-elInput.addEventListener('keypress', codeChar, false);
-
+    $startConvert.on('submit', function(e) {   //evento: pressione sul tasto Convert!, invio della stringa
+        $charConverted = $('li#converted');     //puntatore del risultato precedente
+        $charConverted.remove();                //elimina il risultato precedente
+        e.preventDefault();                     
+        var inputForm = $inputChar.val();       //stringa inserita nell'input
+        var len = inputForm.length;             
+        $reponse.prepend('<li id = "converted">il codice ASCII di ' + inputForm.charAt(0) + ' &egrave : '
+            + inputForm.charCodeAt(0) + '</li>'); //inserisci la conversione del primo carattere
+        for (var i = 1; i < len; i++) {           //se ci sono altri caratteri estraine uno ad uno
+            var $lastChar = $('li:last');
+            $lastChar.after('<li id = "converted">il codice ASCII di ' + inputForm.charAt(i) + ' &egrave : '
+                + inputForm.charCodeAt(i) + '</li>');   //e fornisci il codice ASCII di ognuno in una lista
+        }
+    });
+});
